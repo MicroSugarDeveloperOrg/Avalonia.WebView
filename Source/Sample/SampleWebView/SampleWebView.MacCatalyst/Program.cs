cf78d8bc4350f9f4e3cbbb81e098228e47de3436
wufeng <wufeng4728704@hotmail.com>
2023-05-25 10:50:59 +0800

    changed

--- Source/Sample/SampleWebView/SampleWebView.MacCatalyst/Program.cs
+++ Source/Sample/SampleWebView/SampleWebView.MacCatalyst/Program.cs
@@ -10,8 +10,12 @@ internal class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
+    public static void Main(string[] args)
+    {
+        UIApplication.Main(args);
+        BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
