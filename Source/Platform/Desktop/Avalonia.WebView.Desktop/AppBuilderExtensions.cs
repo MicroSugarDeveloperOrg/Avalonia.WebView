655215a46bc5d475e37102fc77417fdeac52b456
MicroSugar <wufeng4728704@hotmail.com>
2023-05-29 18:45:35 +0800

    use wkwebview

--- Source/Platform/Desktop/Avalonia.WebView.Desktop/AppBuilderExtensions.cs
+++ Source/Platform/Desktop/Avalonia.WebView.Desktop/AppBuilderExtensions.cs
@@ -1,7 +1,8 @@
 ﻿#if __WINDOWS__
 using Avalonia.WebView.Windows;
 # elif __OSX__
-using Avalonia.WebView.Mac;
+using Avalonia.WebView.MacCatalyst;
+//using Avalonia.WebView.Mac;
 # elif __LINUX__
 using Avalonia.WebView.Linux;
 #endif
@@ -15,7 +16,8 @@ public static class AppBuilderExtensions
 #if __WINDOWS__
         builder.UseWindowWebView();
 #elif __OSX__
-        builder.UseOSXWebView();
+        builder.UseMacCatalystWebView();
+        //builder.UseOSXWebView();
 #elif __LINUX__
         builder.UseLinuxWebView();
 #endif
