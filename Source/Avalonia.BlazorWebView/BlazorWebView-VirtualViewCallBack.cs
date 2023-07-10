66abe355233cfee7a72aadbafc53b9dbb22cbc90
MicroSugar <wufeng4728704@hotmail.com>
2023-07-10 11:59:06 +0800

    changed

--- Source/Avalonia.BlazorWebView/BlazorWebView-VirtualViewCallBack.cs
+++ Source/Avalonia.BlazorWebView/BlazorWebView-VirtualViewCallBack.cs
@@ -2,30 +2,36 @@
 
 partial class BlazorWebView  
 {
-    bool IVirtualWebViewControlCallBack.PlatformWebViewCreating(object? sender, WebViewCreatingEventArgs arg)
-    {
-        return true;
-    }
-
     void IVirtualWebViewControlCallBack.PlatformWebViewCreated(object? sender, WebViewCreatedEventArgs arg)
     {
+        WebViewCreated?.Invoke(sender, arg);
     }
 
-    bool IVirtualWebViewControlCallBack.PlatformWebViewNavigationStarting(object? sender, WebViewUrlLoadingEventArg arg)
+    bool IVirtualWebViewControlCallBack.PlatformWebViewCreating(object? sender, WebViewCreatingEventArgs arg)
     {
+        WebViewCreating?.Invoke(sender, arg);
         return true;
-    }
-
-    void IVirtualWebViewControlCallBack.PlatformWebViewNavigationCompleted(object? sender, WebViewUrlLoadedEventArg arg)
-    {
     }
 
     void IVirtualWebViewControlCallBack.PlatformWebViewMessageReceived(object? sender, WebViewMessageReceivedEventArgs arg)
     {
+        WebMessageReceived?.Invoke(sender, arg);
+    }
+
+    void IVirtualWebViewControlCallBack.PlatformWebViewNavigationCompleted(object? sender, WebViewUrlLoadedEventArg arg)
+    {
+        NavigationCompleted?.Invoke(sender, arg);
+    }
+
+    bool IVirtualWebViewControlCallBack.PlatformWebViewNavigationStarting(object? sender, WebViewUrlLoadingEventArg arg)
+    {
+        NavigationStarting?.Invoke(sender, arg);
+        return true;
     }
 
     bool IVirtualWebViewControlCallBack.PlatformWebViewNewWindowRequest(object? sender, WebViewNewWindowEventArgs arg)
     {
+        WebViewNewWindowRequested?.Invoke(sender, arg);
         return true;
     }
 }
---
+++ Source/Sample/SampleBlazorWebView/SampleBlazorWebViewShared/Working/JsWorking.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+using System.Xml.Serialization;
+
+namespace SampleBlazorWebViewShared.Working;
+public class JsWorking
+{
+    [JSInvokable]
+    public static Task<int[]> ReturnArrayAsync()
+    {
+        Console.WriteLine(new int[] { 1,2,3 }); 
+        return Task.FromResult(new int[] { 1,2,3 });
+    }
+
+}
