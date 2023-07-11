9ea59b376edb815d159accb7e710d58b33310a76
MicroSugar <wufeng4728704@hotmail.com>
2023-07-11 13:44:41 +0800

    update 2 avalonia 11.0.0

--- Source/Sample/SampleWebView/SampleWebView.Android/MainActivity.cs
+++ Source/Sample/SampleWebView/SampleWebView.Android/MainActivity.cs
@@ -1,9 +1,21 @@
-﻿using Android.App;
-using Android.Content.PM;
+﻿using Android.Content.PM;
+using Avalonia;
 using Avalonia.Android;
+using Avalonia.ReactiveUI;
+using Avalonia.WebView.Android;
 
 namespace SampleWebView.Android;
-[Activity(Label = "SampleWebView.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-public class MainActivity : AvaloniaMainActivity
+[Activity(Label = "SampleWebView.Android",
+          Theme = "@style/MyTheme.NoActionBar",
+          Icon = "@drawable/icon",
+          MainLauncher = true,
+          ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
 {
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+                   .UseReactiveUI()
+                   .UseAndroidWebView();
+    }
 }
