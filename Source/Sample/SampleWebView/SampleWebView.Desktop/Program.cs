1260dbbf17463c7d478c90b82217f06098787bd0
MicroSugar <wufeng4728704@hotmail.com>
2023-07-21 21:56:53 +0800

    add antdesign font

--- Source/Sample/SampleWebView/SampleWebView.Desktop/Program.cs
+++ Source/Sample/SampleWebView/SampleWebView.Desktop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using AntDesign.Toolkit;
 using Avalonia.ReactiveUI;
 using Avalonia.WebView.DesktopX;
 
@@ -16,9 +17,9 @@ internal class Program
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .With(new X11PlatformOptions() {})
-            .LogToTrace()
+            .UsePlatformDetect() 
+            .LogToTrace()  
             .UseReactiveUI()
+            .UseAntDesignToolkit()
             .UseDesktopWebView();
 }
